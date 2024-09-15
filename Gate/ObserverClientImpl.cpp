e4a196f45fd75b8402ce7530ed73b36f9e4e6270
wnmqaubb08 <376452314@qq.com>
2024-09-15 10:32:46 +0800

    解决乱码问题

--- Gate/ObserverClientImpl.cpp
+++ Gate/ObserverClientImpl.cpp
@@ -54,8 +54,8 @@ CObserverClientImpl::CObserverClientImpl(asio::io_service& io_, const std::strin
     package_mgr_.register_handler(LSPKG_ID_S2C_LOG, [this](const RawProtocolImpl& package, const msgpack::v1::object_handle& raw_msg) {
         try
         {
-            auto req = raw_msg.get().as<ProtocolLSLCLogPrint>();
-            auto msg = Utils::String::to_utf8(req.text);
+			auto req = raw_msg.get().as<ProtocolLSLCLogPrint>();
+			auto msg = req.text;
 #ifdef GATE_ADMIN
             if (!req.silence)
             {
@@ -88,7 +88,7 @@ CObserverClientImpl::CObserverClientImpl(asio::io_service& io_, const std::strin
         try
         {
 			auto req = raw_msg.get().as<ProtocolOBS2OBCLogPrint>();
-			auto msg = Utils::String::to_utf8(req.text);
+			auto msg = req.text;
             if (!req.silence)
             {
                 LogPrint(ServiceLog, TEXT("%s"), msg.c_str());
