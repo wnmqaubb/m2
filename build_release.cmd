f71669451fb80df396a9287963f1f9e4704e4167
wnmqaubb08 <376452314@qq.com>
2024-09-24 19:27:09 +0800

    自动编译打包

---
+++ build_release.cmd
@@ -0,0 +1,39 @@
+@ECHO OFF
+set InstallerPath="D:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE"
+if not exist "%InstallerPath%\devenv.exe" echo "VS2022 not found"&&exit /B 0
+set "Path=%Path%;%InstallerPath%;D:\tool\52pojie\Tools\Packers\VMProtect Ultimate3.4;%ProgramFiles(x86)%\7-Zip"
+set ReleaseDir=.\build\bin\Release\Win32
+set OutputDir=.\build\bin\%2
+set PATH=C:\Program Files\7-Zip\;D:\tool\52pojie\Tools\Packers\VMProtect Ultimate3.4\;%PATH%
+mkdir %OutputDir%
+
+::"D:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe" AntiCheat.sln /build "Release|x86"
+::"D:\Program Files\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\devenv.exe" Tools/PackerTool/packer_tool.vcxproj /rebuild "Release|x86"
+
+
+::echo ***************构建方案***************
+::devenv.exe AntiCheat.sln /rebuild "Release2|x86"
+
+set gm_server=%OutputDir%gm_server\%2
+set admin_server=%OutputDir%admin_server\%2
+
+::echo ***************构建方案***************
+echo D|xcopy %ReleaseDir%\Service.exe %gm_server% /y
+echo D|xcopy %ReleaseDir%\LogicServer.exe %gm_server% /y
+echo D|xcopy %ReleaseDir%\Gate.exe %gm_server% /y
+
+::echo ***************gm_server***************
+VMProtect_Con.exe %ReleaseDir%\Service.exe %gm_server%\Service.exe -pf ServiceRelease.vmp
+VMProtect_Con.exe %ReleaseDir%\LogicServer.exe %gm_server%\LogicServer.exe -pf LogicRelease.vmp
+VMProtect_Con.exe %ReleaseDir%\Gate.exe %gm_server%\Gate.exe -pf GateRelease.vmp
+echo D|xcopy %ReleaseDir%\plugin\* %gm_server%plugin\ /y
+
+::echo ***************admin_server***************
+echo F|xcopy %ReleaseDir%\AdminGate.exe %admin_server%AdminGate.exe /y
+echo F|xcopy %ReleaseDir%\packer.exe %admin_server%packer.exe /y
+echo F|xcopy %ReleaseDir%\packer_tool.exe %admin_server%packer_tool.exe /y
+echo F|xcopy %ReleaseDir%\NewClient.dll %admin_server%NewClient.dll /y
+echo F|xcopy %ReleaseDir%\stage_1_payload.bin %admin_server%stage_1_payload.bin /y
+echo F|xcopy %ReleaseDir%\stage_2_payload.bin %admin_server%stage_2_payload.bin /y
+
+pause
--- make_zip.bat
+++ make_zip.bat
@@ -3,49 +3,35 @@ set PdbDir=.\build\pdb
 set OutputDir=.\build\zip\%2
 set PATH=C:\Program Files\7-Zip\;D:\tool\52pojie\Tools\Packers\VMProtect Ultimate3.4\;%PATH%
 mkdir %OutputDir%
+set gm_server=.\build\bin\gm_server
+set admin_server=.\build\bin\admin_server
+set gm_server_zip=%OutputDir%gm_server
+set admin_server_zip=%OutputDir%admin_server
 
+mkdir %gm_server_zip%
+mkdir %admin_server_zip%
 
-echo D|xcopy %ReleaseDir%\Service.exe %OutputDir%\gm_server\ /y
-echo D|xcopy %ReleaseDir%\LogicServer.exe %OutputDir%\gm_server\ /y
-echo D|xcopy %ReleaseDir%\Gate.exe %OutputDir%\gm_server\ /y
+echo F|xcopy %gm_server%\Service.exe %gm_server_zip%\Service.exe /y
+echo F|xcopy %gm_server%\LogicServer.exe %gm_server_zip%\LogicServer.exe /y
+echo F|xcopy %gm_server%\Gate.exe %gm_server_zip%\Gate.exe /y
+echo F|xcopy %gm_server%\serial_demo.txt %gm_server_zip%\serial.txt /y
 
-VMProtect_Con.exe %ReleaseDir%\Service.exe %OutputDir%\gm_server\Service.exe -pf ServiceRelease.vmp
-VMProtect_Con.exe %ReleaseDir%\LogicServer.exe %OutputDir%\gm_server\LogicServer.exe -pf LogicRelease.vmp
-VMProtect_Con.exe %ReleaseDir%\Gate.exe %OutputDir%\gm_server\Gate.exe -pf GateRelease.vmp
+echo D|xcopy %gm_server%\plugin\* %gm_server_zip%\plugin\ /y
 
-mkdir .\build\bin\gm_server
-mkdir .\build\bin\admin_server
-echo D|xcopy %OutputDir%\gm_server\Service.exe .\build\bin\gm_server\Service.exe /y
-echo D|xcopy %OutputDir%\gm_server\LogicServer.exe .\build\bin\gm_server\LogicServer.exe /y
-echo D|xcopy %OutputDir%\gm_server\Gate.exe .\build\bin\gm_server\Gate.exe /y
-echo D|xcopy %ReleaseDir%\plugin\* .\build\bin\gm_server\plugin\ /y
-echo F|xcopy %ReleaseDir%\AdminGate.exe .\build\bin\admin_server\AdminGate.exe /y
-echo F|xcopy %ReleaseDir%\packer.exe .\build\bin\admin_server\packer.exe /y
-echo F|xcopy %ReleaseDir%\packer_tool.exe .\build\bin\admin_server\packer_tool.exe /y
-echo F|xcopy %ReleaseDir%\NewClient.dll .\build\bin\admin_server\NewClient.dll /y
-echo F|xcopy %ReleaseDir%\stage_1_payload.bin .\build\bin\admin_server\stage_1_payload.bin /y
-echo F|xcopy %ReleaseDir%\stage_2_payload.bin .\build\bin\admin_server\stage_2_payload.bin /y
-
-
-echo D|xcopy %ReleaseDir%\plugin\* %OutputDir%\gm_server\plugin\ /y
-echo F|xcopy %ReleaseDir%\AdminGate.exe %OutputDir%\admin_server\AdminGate.exe /y
-echo F|xcopy %ReleaseDir%\packer.exe %OutputDir%\admin_server\packer.exe /y
-echo F|xcopy %ReleaseDir%\packer_tool.exe %OutputDir%\admin_server\packer_tool.exe /y
-echo F|xcopy %ReleaseDir%\NewClient.dll %OutputDir%\admin_server\NewClient.dll /y
-echo F|xcopy %ReleaseDir%\stage_1_payload.bin %OutputDir%\admin_server\stage_1_payload.bin /y
-echo F|xcopy %ReleaseDir%\stage_2_payload.bin %OutputDir%\admin_server\stage_2_payload.bin /y
-
-echo F|xcopy %ReleaseDir%\*.exe %OutputDir%\all\ /y
-echo F|xcopy %ReleaseDir%\*.dll %OutputDir%\all\ /y
-echo F|xcopy %ReleaseDir%\*.bin %OutputDir%\all\ /y
+echo F|xcopy %ReleaseDir%\*.exe %OutputDir%all\ /y
+echo F|xcopy %ReleaseDir%\*.dll %OutputDir%all\ /y
+echo F|xcopy %ReleaseDir%\*.bin %OutputDir%all\ /y
 echo F|xcopy %PdbDir%\*.pdb %OutputDir%\all\ /y
-::7z a -tzip %OutputDir%\pdbs.zip %OutputDir%\all\*
-::7z a -tzip %OutputDir%\admin_server.zip %OutputDir%\admin_server\*
-::7z a -tzip %OutputDir%\gm_server.zip %OutputDir%\gm_server\*
-::7z a -tzip %OutputDir%\%1.zip %OutputDir%\admin_server.zip %OutputDir%\gm_server.zip
+
+7z a -tzip %OutputDir%pdbs.zip %OutputDir%all\*
+::7z a -tzip %admin_server_zip%.zip %admin_server_zip%\*
+7z a -tzip %gm_server_zip%.zip %gm_server_zip%\*
+7z a -tzip %OutputDir%%1.zip %admin_server_zip%.zip %gm_server_zip%.zip
 
 ::del %OutputDir%\gm_server.zip
-::del %OutputDir%\admin_server.zip
-rmdir /q /s %OutputDir%\all
-rmdir /q /s %OutputDir%\admin_server
-rmdir /q /s %OutputDir%\gm_server
+::del %admin_server_zip%.zip
+rmdir /q /s %OutputDir%all
+::rmdir /q /s %admin_server_zip%
+rmdir /q /s %gm_server_zip%
+
+pause
