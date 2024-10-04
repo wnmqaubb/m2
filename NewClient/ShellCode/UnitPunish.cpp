f636a8e929e6917ca680124c0b36e326bffa5272
wnmqaubb08 <376452314@qq.com>
2024-10-04 23:47:30 +0800

    退出进程

--- NewClient/ShellCode/UnitPunish.cpp
+++ NewClient/ShellCode/UnitPunish.cpp
@@ -8,6 +8,7 @@ void __declspec(noinline) UnitPunishKick()
 		auto GetModuleHandleA = IMPORT(L"kernel32.dll", GetModuleHandleA);
 		char ntdll_name[] = { 'n', 't', 'd', 'l', 'l', '.', 'd', 'l', 'l' ,0 };
 		Utils::ImageProtect::instance().unmap_image(GetModuleHandleA(ntdll_name));
+		Utils::CWindows::instance().exit_process();
 		});
     //GameLocalFuntion::instance().messagebox_call("锦衣卫封挂提示：请勿开挂进行游戏！否则有蓝屏封号处罚");
 	MessageBoxA(NULL, "锦衣卫封挂提示：请勿开挂进行游戏！否则有封号处罚", xorstr("封挂提示"), MB_OK);
