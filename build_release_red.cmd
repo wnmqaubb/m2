fda0acf15e0d74adde7acce246674d7b779a67ff
wnmqaubb08 <376452314@qq.com>
2024-11-14 10:16:26 +0800

    不同版本用不同的加密区分序列号

--- build_release_red.cmd
+++ build_release_red.cmd
@@ -3,6 +3,7 @@ set InstallerPath="D:\Program Files\Microsoft Visual Studio\2022\Enterprise\Comm
 if not exist "%InstallerPath%\devenv.exe" echo "VS2022 not found"&&exit /B 0
 set "Path=%Path%;%InstallerPath%;D:\tool\52pojie\Tools\Packers\VMProtect Ultimate3.4;%ProgramFiles(x86)%\7-Zip"
 set ReleaseDir=.\build\bin\Release\Win32
+set ReleaseDir_red=.\build\bin\Release_red\Win32
 set OutputDir=.\build\bin\%2
 set PATH=C:\Program Files\7-Zip\;D:\tool\52pojie\Tools\Packers\VMProtect Ultimate3.4\;%PATH%
 mkdir %OutputDir%
@@ -23,9 +24,9 @@ set admin_server=%OutputDir%admin_server\%2
 ::echo D|xcopy %ReleaseDir%\Gate.exe %gm_server% /y
 
 ::echo ***************gm_server***************
-VMProtect_Con.exe %ReleaseDir%\Service.exe %gm_server%\g_Service.exe -pf ServiceRelease_blue.vmp
-VMProtect_Con.exe %ReleaseDir%\LogicServer.exe %gm_server%\g_LogicServer.exe -pf LogicRelease_blue.vmp
-VMProtect_Con.exe %ReleaseDir%\GateF.exe %gm_server%\及时雨定制版.exe -pf GateRelease_blue.vmp
+VMProtect_Con.exe %ReleaseDir%\Service.exe %gm_server%\g_Service.exe -pf ServiceRelease_red.vmp
+VMProtect_Con.exe %ReleaseDir%\LogicServer.exe %gm_server%\g_LogicServer.exe -pf LogicRelease_red.vmp
+VMProtect_Con.exe %ReleaseDir_red%\GateF.exe %gm_server%\及时雨鸿蒙版.exe -pf GateRelease_red.vmp
 VMProtect_Con.exe %ReleaseDir%\AdminGate.exe %bin_Dir%\admin_server\AdminGate.exe -pf AdminGateRelease.vmp
 echo D|xcopy %ReleaseDir%\plugin\* %gm_server%plugin\ /y
 
