331b2381ae73f5d5dc4f4ba43694fd4a3e0f51ff
wnmqaubb <18924589992@189.cn>
2025-06-12 10:24:39 +0800

    1

--- Gate_F/CAntiCheatDlg.cpp
+++ Gate_F/CAntiCheatDlg.cpp
@@ -28,6 +28,7 @@ BEGIN_MESSAGE_MAP(CAntiCheatDlg, CDialogEx)
 	ON_WM_CONTEXTMENU()
 	//ON_COMMAND(ID_ADD_TO_WHITELIST, OnAddToWhiteList)
 	ON_COMMAND(ID_REMOVE_FROM_WHITELIST, OnRemoveFromWhiteList)
+    ON_WM_SIZE()
 END_MESSAGE_MAP()
 
 CAntiCheatDlg::~CAntiCheatDlg()
@@ -37,6 +38,30 @@ CAntiCheatDlg::~CAntiCheatDlg()
 BOOL CAntiCheatDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
+
+    // 获取初始窗口大小
+    CRect rect;
+    GetClientRect(&rect);
+    m_originalSize = rect.Size();
+
+    // 初始化布局信息
+    CWnd* pChild = GetWindow(GW_CHILD);
+    while (pChild)
+    {
+        ControlLayoutInfo info;
+        pChild->GetWindowRect(&info.originalRect);
+        ScreenToClient(&info.originalRect);
+        info.nID = pChild->GetDlgCtrlID();
+
+        // 设置锚定方式
+        {
+            info.anchor = AnchorStyle::LEFT | AnchorStyle::TOP;
+        }
+
+        m_layoutInfos.push_back(info);
+        pChild = pChild->GetNextWindow();
+    }
+
 	m_current_list = &m_list_white_machine;
 	ReadFileAndShowList("机器码白名单.txt");
 	m_current_list = &m_list_white_ip;
@@ -164,4 +189,74 @@ void CAntiCheatDlg::ReadFileAndShowList(const std::string& filePath)
 		}
 		inFile.close();
 	}
+}
+
+
+void CAntiCheatDlg::OnSize(UINT nType, int cx, int cy)
+{
+    CDialogEx::OnSize(nType, cx, cy);
+
+    if (nType == SIZE_MINIMIZED || cx <= 0 || cy <= 0)
+        return;
+
+    // 计算大小变化量
+    int deltaX = cx - m_originalSize.cx;
+    int deltaY = cy - m_originalSize.cy;
+
+    // 使用延迟窗口定位以获得更好的性能
+    HDWP hdwp = BeginDeferWindowPos((int)m_layoutInfos.size());
+
+    for (auto& info : m_layoutInfos)  // 修改为auto&以便更新原始位置
+    {
+        CWnd* pWnd = GetDlgItem(info.nID);
+        if (!pWnd || !pWnd->GetSafeHwnd())
+            continue;
+
+        CRect newRect = info.originalRect;
+
+        // 根据锚定方式调整位置和大小
+        if ((info.anchor & AnchorStyle::RIGHT) != 0)
+        {
+            if ((info.anchor & AnchorStyle::LEFT) != 0)
+            {
+                // 左右都锚定，调整宽度
+                newRect.right += deltaX;
+            }
+            else
+            {
+                // 只锚定右边，移动位置
+                newRect.left += deltaX;
+                newRect.right += deltaX;
+            }
+        }
+
+        if ((info.anchor & AnchorStyle::BOTTOM) != 0)
+        {
+            if ((info.anchor & AnchorStyle::TOP) != 0)
+            {
+                // 上下都锚定，调整高度
+                newRect.bottom += deltaY;
+            }
+            else
+            {
+                // 只锚定下边，移动位置
+                newRect.top += deltaY;
+                newRect.bottom += deltaY;
+            }
+        }
+
+        hdwp = DeferWindowPos(hdwp, pWnd->m_hWnd, NULL,
+                              newRect.left, newRect.top,
+                              newRect.Width(), newRect.Height(),
+                              SWP_NOZORDER);
+
+        // 更新控件的原始位置信息
+        info.originalRect = newRect;  // 添加这行以更新控件位置
+    }
+
+    if (hdwp)
+        EndDeferWindowPos(hdwp);
+
+    // 更新原始大小为当前大小
+    m_originalSize = CSize(cx, cy);
 }
